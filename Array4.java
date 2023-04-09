37f205abad05c4c2c5eb7c12354d001651e6dad9
Jbraval <128069825+Jbraval@users.noreply.github.com>
2023-04-09 18:40:07 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,12 @@
+
+public class Array4 {
+    public static void main(String[] args) {
+        int [] arr ={2,-3,5,8,1,0,-4};
+        int min=arr[0];
+        for (int i = 1; i < arr.length ; i++) {
+            min=Math.min(min,arr[i]);
+        }
+        System.out.println("Minimum value: "+min);
+
+    }
+}
